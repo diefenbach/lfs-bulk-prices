38757b7ad67f34de9c379ee4705dc7fd35f3d9a2
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-11-29 12:20:02 +0100

    Changed event for bulk price calculation to keyup.

--- lfs_bulk_prices/static/lfs_bulk_prices/frontend.js
+++ lfs_bulk_prices/static/lfs_bulk_prices/frontend.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(document).on("change", ".product input[name=quantity]", function() {
+    $(document).on("keyup", ".product input[name=quantity]", function() {
         var url = $(this).attr("bulk_prices_update_url");
         var amount = $(this).val();
         $.get(url, {"amount": amount}, function(data) {
