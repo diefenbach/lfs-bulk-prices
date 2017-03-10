ee4a41a83c0c3de9be90b17089136babc27be151
Kai Diefenbach <kai.diefenbach@iqpp.de>
2017-03-10 06:30:08 +0100

    Remove login/logout views

--- lfs_bulk_prices/urls.py
+++ lfs_bulk_prices/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from . import views
 
-urlpatterns = patterns('django.contrib.auth.views',
-    url('^login', "login", {"template_name": "lfs_bulk_prices/login.html"}, name='lfs_bulk_prices_login'),
-    url('^logout', "logout", {"template_name": "lfs_bulk_prices/logged_out.html"}, name='lfs_bulk_prices_logout'),
-)
-
-urlpatterns += patterns('lfs_bulk_prices.views',
-    url(r'^$', "lfs_bulk_prices_update", name="lfs_bulk_prices_update"),
-    url(r'^update-prices/(?P<product_id>\d*)$', "update_prices", name="lfs_bulk_prices_update_prices"),
-)
+urlpatterns = [
+    url(r'^$', views.lfs_bulk_prices_update, name="lfs_bulk_prices_update"),
+    url(r'^update-prices/(?P<product_id>\d*)$', views.update_prices, name="lfs_bulk_prices_update_prices"),
+]
